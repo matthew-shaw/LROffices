306675c64cc3f3b27d6ca59f73fcbb90fbf5dce3
matthew-shaw <matthew.shaw@digital.landregistry.gov.uk>
2014-11-10 10:33:22 +0000

    add-office-data

---
+++ js/officesjson.js
@@ -0,0 +1,229 @@
+var lrOffices = {
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -3.013679,
+                    53.394709
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Birkenhead",
+                "address": "Rosebrae Court Woodside Ferry Approach Birkenhead Merseyside CH41 6DU",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#birkenhead"
+            },
+            "id": 1
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -1.57424,
+                    52.40561
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Coventry",
+                "address": "Leigh Court Torrington Avenue Tile Hill Coventry CV4 9XZ",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#coventry"
+            },
+            "id": 2
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -0.098663,
+                    51.37888
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Croydon",
+                "address": "Trafalgar House 1 Bedford Park Croydon CR0 2AQ",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#croydon"
+            },
+            "id": 3
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -1.592696,
+                    54.786725
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Durham",
+                "address": "Southfield House Southfield Way Durham DH1 5TR",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#durham"
+            },
+            "id": 4
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.239092,
+                    51.86492
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Gloucester",
+                "address": "Twyver House Bruton Way Gloucester GL1 1DQ",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#gloucester"
+            },
+            "id": 5
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -0.350597,
+                    53.747578
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Hull",
+                "address": "Earle House Colonial Street Hull HU2 8JN",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#hull"
+            },
+            "id": 6
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.91162,
+                    53.744431
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Fylde",
+                "address": "Wrea Brook Court Lytham Road Warton Preston PR4 1TE",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#fylde"
+            },
+            "id": 7
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -1.142869,
+                    52.632881
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Leicester",
+                "address": "Westbridge Place Leicester LE3 5DR",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#leicester"
+            },
+            "id": 8
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -1.19818,
+                    52.96602
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Nottingham",
+                "address": "Castle Wharf House 2 Canal Street Nottingham NG1 7AU",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#nottingham"
+            },
+            "id": 9
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -0.23814,
+                    52.57411
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Peterborough",
+                "address": "Stuart House West Wing City Road Peterborough PE1 1QF",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#peterborough"
+            },
+            "id": 10
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -4.12594,
+                    50.41273
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Plymouth",
+                "address": "Seaton Court 2 William Prance Road Plymouth PL6 5WS",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#plymouth-office"
+            },
+            "id": 11
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.4561,
+                    52.67766
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Telford",
+                "address": "Parkside Court Hall Park Way Telford TF3 4LR",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#telford"
+            },
+            "id": 12
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -3.912853,
+                    51.651585
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Wales",
+                "address": "Ty Cwm Tawe Phoenix Way Llansamlet Swansea SA7 9FQ",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#wales"
+            },
+            "id": 13
+        },
+        {
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -2.486628,
+                    50.612738
+                ]
+            },
+            "type": "Feature",
+            "properties": {
+                "name": "Weymouth",
+                "address": "Melcombe Court 1 Cumberland drive Weymouth Dorset DT4 9TT",
+                "url": "https://www.gov.uk/government/publications/land-registry-office-addresses/office-addresses#weymouth"
+            },
+            "id": 14
+        }
+    ]
+};
